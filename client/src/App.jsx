9d4c5ab7ab7b0a601305fd31a0423f01e349aca8
Md Rahim Apurbo <111771609+MdRahimApurbo@users.noreply.github.com>
2024-04-22 14:11:06 +0600

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
 
 const App = () => {
   return (
-    <div>
-      <h2>Hello Rakib</h2>
-      
+    <div className="flex flex-row gap-5">
+      <div className="bg-[#F1F2F7] p-5 w-[300px] h-screen">
+        <Navbar />
+      </div>
+      <div>
+        <Content />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className='text-xl'>Create Food</h2>
+            <h2 className='text-xl'>All Food</h2>
+        </div>
+    );
+};
+
+export default Navbar;
