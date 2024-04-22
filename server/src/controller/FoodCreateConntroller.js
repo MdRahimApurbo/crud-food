814c7a9cb359d33d4c1f612184b6583f04210b2d
Md Rahim Apurbo <111771609+MdRahimApurbo@users.noreply.github.com>
2024-04-22 09:43:15 +0600

    Update FoodCreateConntroller.js
    2d

--- server/src/controller/FoodCreateConntroller.js
+++ server/src/controller/FoodCreateConntroller.js
@@ -15,3 +15,75 @@ exports.CreateFood = (req,res)=>{
     })
 
 }
+
+// Read All Food
+
+exports.ReadAllFood = (req,res)=>{
+
+    let query = {}
+    projecttion = {FoodName}
+    FoodModel.find(query,projecttion,(err,data)=> {
+        if(err){
+            res.status(400).json({status:'fail',data:err})
+        }else{
+           res.status(200).json({status:'success',data:data})
+        }
+    })
+
+}
+
+// Read Food
+
+exports.ReadFood = (req,res)=>{
+
+    let id = req.params.id;
+    let query = {_id:id}
+    let reqBody = req.body;
+
+    FoodModel.find(query,(err,data)=> {
+        if(err){
+            res.status(400).json({status:'fail',data:err})
+        }else{
+           res.status(200).json({status:'success',data:data})
+        }
+    })
+
+}
+
+
+// Update Food
+
+exports.UpdateFood = (req,res)=>{
+
+    let id = req.params.id;
+    let query = {_id:id}
+    let reqBody = req.body;
+
+    FoodModel.updateOne(query,reqBody,(err,data)=> {
+        if(err){
+            res.status(400).json({status:'fail',data:err})
+        }else{
+           res.status(200).json({status:'success',data:data})
+        }
+    })
+
+}
+
+
+// Delete Food
+
+exports.DeleteFood = (req,res)=>{
+
+    let id = req.params.id;
+    let query = {_id:id}
+
+
+    FoodModel.remove(query,(err,data)=> {
+        if(err){
+            res.status(400).json({status:'fail',data:err})
+        }else{
+           res.status(200).json({status:'success',data:data})
+        }
+    })
+
+}
