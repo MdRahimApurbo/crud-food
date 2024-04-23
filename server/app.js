1fa93b8e70930ba5617f9c1ac5d29887294dc190
Md Rahim Apurbo <111771609+MdRahimApurbo@users.noreply.github.com>
2024-04-23 18:38:25 +0600

    final

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 // Security Middleware Implement
 app.use(cors(
     {
-        origin:["https:deplay-crud-apurbo-vercel.app"],
+        origin:["https:crud-food-api-vercel.app"],
         methods:['POST','GET'],
         credentials:true
     }
